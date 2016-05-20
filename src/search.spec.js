137b278a58b93fe91d04c17816a7b62ad85d03da
Rob Nelson <rn47@duke.edu>
2016-05-20 15:32:22 -0400

    FDP-2592: more attempts at testing

--- src/search.spec.js
+++ src/search.spec.js
@@ -12,9 +12,7 @@ import * as types from './actions/types'
 
 import configureStoreWithoutLogger from './configureStore'
 
-//import expect, { createSpy, spyOn, isSpy } from 'expect'
 import assert from 'assert'
-
 
 // NOTE: nock throws no module 'fs' found errors
 // tried adding this to webpack-config - no errors at least
@@ -24,7 +22,6 @@ import assert from 'assert'
 import nock from 'nock'
 
 describe("Running a Search", () => {
-  
   var results = {}
   const store = mockStore({ search: []})
   const compoundSearch = { 'allWords': 'medicine' }
@@ -40,19 +37,12 @@ describe("Running a Search", () => {
           classgroup: ["http://vivoweb.org/ontology#vitroClassGrouporganizations"],
           URI: "https://scholars.duke.edu/individual/org50000844",
           ALLTEXT: ["50000844 Medicine"],
-          type: ["http://vivoweb.org/ontology/core#AcademicDepartment"]
+          type: ["http://vivoweb.org/ontology/core#AcademicDepartment"],
+          nameRaw: ["Medicine"]
         }  
       ]
     }
   }
-
-  // NOTE: I couldn't get the standard Redux way to work
-  // see WritingTests link way above
-
-  //const expectedActions = [
-  //  { type: types.REQUEST_SEARCH },
-  //  { type: types.RECEIVE_SEARCH, body: solrJson }
-  //]
 
   // FIXME: this 'nock' thing is supposed to proxy the call to Solr 
   // (so the test doesn't actually have to hit solr)
@@ -67,10 +57,9 @@ describe("Running a Search", () => {
     .reply(200, { body: solrJson })
  
 
-  // NOTE: needed to do this to call expect() and test things 
+  // NOTE: needed to do this (calling done())
   // found idea here: https://volaresystems.com/blog/post/2014/12/09/Testing-async-calls-with-Jasmine
   beforeEach(function(done) {
-    //  
     store.dispatch(actions.fetchSearch(compoundSearch))
       .then((results) => {
         results = results
@@ -80,24 +69,15 @@ describe("Running a Search", () => {
   })
 
   it("should RECEIVE_SEARCH when running search", () => {
-    // NOTE: here's an example supposely using isomporphic-search
-    // and nock -- 
-    // https://github.com/node-nock/nock/issues/399
-
-    // FIXME: this seems like an odd and/or useless test specification
-    // just says redux is routing correctly after call to actions.fetchSearch()
-    expect(store.getActions()[0].type).toEqual(types.REQUEST_SEARCH)
-    expect(store.getActions()[1].type).toEqual(types.RECEIVE_SEARCH)
-  
+    assert(store.getActions()[0].type == types.REQUEST_SEARCH)
+    assert(store.getActions()[1].type == types.RECEIVE_SEARCH)
   })
 
  
   it ("should return at least ONE doc", () => {
-    //expect(store.getActions()[1].results.response.docs.length).toBeGreaterThan(0)
     assert(store.getActions()[1].results.response.docs.length > 0)
     // NOTE: if it were using 'nock' this would be true
-    //expect(store.getActions()[1].results.response.docs.length).toEqual(1)
-  
+    //assert(store.getActions()[1].results.response.docs.length == 1)
   })
 
 
@@ -133,44 +113,56 @@ describe('search reducer', () => {
 
 })
 
+// tried following patterns in this (but largely did not succeed):
 // http://engineering.pivotal.io/post/tdding-react-and-redux/
  
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
 import { Provider } from 'react-redux'
+import { Router, Route } from 'react-router'
+//import { createStore } from 'redux'
+//import { applyMiddleware } from 'redux'
+//import thunkMiddleware from 'redux-thunk'
+
+import { syncHistoryWithStore } from 'react-router-redux'
+import { createHistory } from 'history';
+import { useRouterHistory } from 'react-router';
 
 import ScholarsSearch from './containers/ScholarsSearch'
-import ScholarsSearchApp from './containers/ScholarsSearchApp'
 
+// NOTE: these are 'unconnected' versions - the problem is SearchTab(s) and PagingPanel
+// are 'connected' -- so even trying to test SearchResults as 'shallow' render
+// (see https://simonsmith.io/unit-testing-react-components-without-a-dom/) is problematic
+//
+//import { ScholarsSearchApp } from './containers/ScholarsSearchApp'
+//import { SearchForm } from './components/SearchForm'
+//import { SearchResults } from './components/SearchResults'
 
-import SearchForm from './components/SearchForm'
-import SearchResults from './components/SearchResults'
+// NOTE: these would be the 'connected' versions
+import  ScholarsSearchApp from './containers/ScholarsSearchApp'
+import  SearchForm from './components/SearchForm'
+import  SearchResults from './components/SearchResults'
 
-import { Router, Route } from 'react-router'
+//import { mainReducer, searchReducer } from './reducers/search'
 
-import { createStore } from 'redux'
-
-import { mainReducer, searchReducer } from './reducers/search'
-
-import { applyMiddleware } from 'redux'
-import thunkMiddleware from 'redux-thunk'
-
-//<Route path="/" component={ScholarsSearchApp}  onEnter={onRoutesEnter} >
- 
-describe("<ScholarsSearch />", () => {
+describe("<Routing />", () => {
 
   var app 
   var store 
   var compoundSearch
 
   beforeEach(function(done) {
-    // FIXME: tried to create simpler store, but couldn't get to work
-    //store = mockStore({ search: {results: {}, searchFields: {}}}, applyMiddleware(thunkMiddleware))
-    //store = createStore(reducers.searchReducer, { search: {isFetching: false, results: {}}, applyMiddleware(thunkMiddleware))
     store = configureStoreWithoutLogger()
-     
-    app = TestUtils.renderIntoDocument(<Provider store={store}><div><SearchForm/><SearchResults/></div></Provider>);
+
+    const browserHistory = useRouterHistory(createHistory)({
+      basename: '/scholars_search'
+    });
+
+    const history = syncHistoryWithStore(browserHistory, store)
+
+    app = TestUtils.renderIntoDocument(<Provider store={store}><Router history={history}><ScholarsSearchApp /></Router></Provider>)
+       
     compoundSearch = { 'allWords': 'medicine'}
  
     store.dispatch(actions.fetchSearch(compoundSearch))
@@ -182,40 +174,58 @@ describe("<ScholarsSearch />", () => {
   })
 
 
-  it('passes down search results', function() {
-     var child =TestUtils.findRenderedComponentWithType(app, SearchResults)
+  it('puts search parameters in the route path', function() {
+    var child =TestUtils.findRenderedComponentWithType(app, Router)
+
+    // FIXME: I don't understand how I can git location.query here ?? (to test) 
+    console.debug(child)
+
+  })
+
+
+})
+
+describe("<ScholarsSearch />", () => {
+
+  var app 
+  var store 
+  var compoundSearch
+
+  beforeEach(function(done) {
+    store = configureStoreWithoutLogger()
+    app = TestUtils.renderIntoDocument(<Provider store={store}><SearchResults/></Provider>)
+
+    compoundSearch = { 'allWords': 'medicine'}
  
+    store.dispatch(actions.fetchSearch(compoundSearch))
+      .then((results) => {
+        results = results
+        done()
+     })
+ 
+  })
+
+
+  it('adds search fields to the store', function() {
+     var child = TestUtils.findRenderedComponentWithType(app, SearchResults)
+      
      const node = ReactDOM.findDOMNode(child)
      const list = node.querySelectorAll(".search-results-table")
 
      assert(list[0].children.length > 0)
 
-     const state = store.getState()
-
-     assert(state['search'].searchFields == compoundSearch)
-
   })
+
 })
 
 // tests to write?  
 //
-// 1. If you hit search - it puts the searchFields in the store
-// 2. If you hit search - it updates the Route path (url)
-// 3. If I get results on a tab, it should show page number, back <-> next (if pages > PAGE_ROWS)
+// 1. If you hit search it updates the Route path (url) with parameters
+// 2. If I get results on a tab, it should show page number, back <-> next (if pages > PAGE_ROWS)
  
 // use this?
 // https://jeremydmiller.com/2016/01/26/how-im-testing-reduxified-react-components/
 
-// describe("Paging Results", () => {
-// 
-//
-//})
-
-// FIXME: use Simulate somewhere?
-// https://facebook.github.io/react/docs/test-utils.html
-// <button ref="button">...</button>
-// var node = this.refs.button;
-// ReactTestUtils.Simulate.click(node);
 
 
 
