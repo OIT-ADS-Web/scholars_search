cf0925ac489e787f99381e51635cf697fa183bca
Rob Nelson <rn47@duke.edu>
2016-05-20 11:06:40 -0400

    adding deploy files

---
+++ deploy.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+set -o errexit
+
+ENVIRONMENT=${1:-"acceptance"}
+deploy_dir="/srv/web/apps/scholars_search"
+
+case "$ENVIRONMENT" in
+  acceptance)
+    SERVERS=("scholars-web-test-04.oit.duke.edu" "scholars-web-test-05.oit.duke.edu")
+    ;;
+  #production)
+  # SERVERS=("scholars-web-04.oit.duke.edu" "scholars-web-05.oit.duke.edu")
+  # ;;
+  *)
+    echo "Usage: $0 {acceptance|production}"
+    exit 1
+esac
+
+
+for SERVER in "${SERVERS[@]}"; do
+  echo "deploying to $ENVIRONMENT server: $SERVER..."
+
+  echo "creating deploy directory on $SERVER..."
+  ssh tomcat@$SERVER "[ -d $deploy_dir ] || mkdir $deploy_dir"
+ 
+  #echo "build dist with webpack"
+  #NODE_ENV=$ENVIRONMENT npm run build $ENVIRONMENT 
+
+  echo "rsyncing app.js to $SERVER..."
+  rsync -avz dist/app.js tomcat@$SERVER:$deploy_dir/app.js
+ 
+  echo "rsyncing index.html to $SERVER..."
+  rsync -av dist/index.html tomcat@$SERVER:$deploy_dir/index.html
+   
+  echo "rsyncing index.html to $SERVER..."
+  rsync -av dist/app.js.map tomcat@$SERVER:$deploy_dir/app.js.map
+
+  echo "rsyncing image files to $SERVER..."
+  rsync -av dist/*.gif tomcat@$SERVER:$deploy_dir/
+
+done
+
