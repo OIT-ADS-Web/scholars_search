076ee5fac465a86b598eb3bb8d55e891bd804be5
Rob Nelson <rn47@duke.edu>
2016-10-13 11:34:59 -0400

    FDP-2736: a) make 'other' tab like the rest of them - no 'Generic' b) hard code some fixes to URIs to create links

--- src/components/tabs/OtherTab.js
+++ src/components/tabs/OtherTab.js
@@ -1,9 +1,52 @@
 import React, { Component } from 'react'
 
-import GenericTab from './GenericTab'
-
 import Facets from '../Facets'
 import HasFacets from '../HasFacets'
+import HasSolrData from '../HasSolrData'
+
+import ScholarsLink from '../ScholarsLink'
+
+class OtherDisplay extends HasSolrData(Component) {
+
+  constructor(props) {
+    super(props)
+    this.doc = this.props.doc;
+    this.highlight = this.props.highlight
+  }
+
+  render() {
+    let geoMatch = /^http:\/\/aims.fao.org\/aos\/geopolitical.owl*/
+
+    let uri = this.URI
+    let isGeo= geoMatch.test(uri)
+
+    if (isGeo) {
+      uri = `https://scholars.duke.edu/individual?uri=${encodeURIComponent(this.URI)}`
+    }
+
+    return (
+         <div className="generic search-result-row" key="{this.docId}">
+            <div className="row">
+              <div className="col-md-12 col-xs-12 col-sm-12"> 
+                 <strong>
+                  <ScholarsLink uri={uri} text={this.name} />
+                </strong>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-12">
+               {this.typeDisplay}
+              </div>
+            </div>
+
+            {this.solrDocDisplay}
+ 
+        </div>
+    )
+  }
+
+}
 
 
 class OtherFacets extends HasFacets(Component) {
@@ -31,19 +74,16 @@ class OtherFacets extends HasFacets(Component) {
 
  }
 
+import Tab from '../Tab'
+
 import { TabFilterer, TabDisplayer } from '../Tab'
 
-import { GenericTabDisplayer, GenericDisplay } from './GenericTab'
+//import { GenericTabDisplayer, GenericDisplay } from './GenericTab'
 
-// got "Super expression must either be null or a function, not undefined"
-// when trying to use this:
-//
-//class OtherTabDisplayer extends GenericTabDisplayer {
 class OtherTabDisplayer extends TabDisplayer {
 
   individualDisplay(doc, highlight) {
-    //return super.individualDisplay(doc, highlight)
-    return <GenericDisplay key={doc.DocId} doc={doc} highlight={highlight}/> 
+    return <OtherDisplay key={doc.DocId} doc={doc} highlight={highlight}/> 
   }
 
   
@@ -65,20 +105,20 @@ class OtherFilterer extends TabFilterer {
 }
 
 
-class OtherTab extends GenericTab {
+class OtherTab extends Tab {
 
   constructor() {
-
     super()
 
     this.id =  "misc"
-    this.filter =  "{!tag=misc}type:((*Award) OR (*Presentation) OR (*geographical_region) OR (*self_governing))"
+    this.filter =  "{!tag=misc}type:((*Award) OR (*Presentation) OR (*ProfessionalActivity) OR (*geographical_region) OR (*self_governing))"
+    // NOTE: this gets *everything else* which is sometimes useful
+    //
     //this.filter = "{!tag=misc}type:(NOT((*Person) OR (*bibo/Document) OR (*Organization) OR (*Grant) OR (*Course) OR (*ArtisticWork) OR (*Concept)))"
  
     this.label =  "Other"
 
     this.displayer = new OtherTabDisplayer()
-
     this.filterer = new OtherFilterer(this.filter)
 
   }
