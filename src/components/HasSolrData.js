f8c6717a56df9a4fdfb07ffab62b9fea26fda60b
Rob Nelson <rn47@duke.edu>
2016-10-20 14:32:02 -0400

    FDP-2736: remove block of SOLR debug data on production (so it can be seen)

--- src/components/HasSolrData.js
+++ src/components/HasSolrData.js
@@ -153,7 +153,7 @@ let HasSolrData = (superclass) => class extends superclass {
 
     // NOTE: so we're not showing this except in development
     // it's just a view of the SOLR doc for debugging purposes
-    if (env == 'production') {
+    if (env == '_production') {
     //if (env == 'production' || env == 'acceptance') {
       return (<span></span>)
     }
