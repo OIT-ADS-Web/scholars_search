771d25dbbfedddc86c3da9853a7193661751c91d
Greg Burton <greg.burton@duke.edu>
2019-03-22 11:13:01 -0400

    FDP-3358: added screen reader label for search field.

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -37,7 +37,6 @@ export default class SearchField extends Component {
     return this.shouldSetInputTextToDefaultValue(nextProps)
   }
 
-
   focus() {
     this.refs.myInput.focus()
   }
@@ -68,22 +67,23 @@ export default class SearchField extends Component {
     const { label, placeholder, defaultValue, autofocus } = this.props
 
     let input = (
-      <input onBlur = {this.handleBlur} type="text" ref="myInput" className="form-control input-sm"  placeholder={placeholder}/>
+      <input onBlur = {this.handleBlur} id="search" type="text" ref="myInput" className="form-control input-sm" placeholder={placeholder}/>
     )
     
     if (autofocus) {
       input = (
-        <input autoFocus={true} onBlur = {this.handleBlur} type="text" ref="myInput" className="form-control input-sm"  placeholder={placeholder}/>
+        <input autoFocus={true} onBlur = {this.handleBlur} id="search" type="text" ref="myInput" className="form-control input-sm" placeholder={placeholder}/>
       )
     }
 
     return (
-         <div className="form-group">
-            <label className="col-sm-4 control-label">{label}</label>
-            <div className="col-sm-8">
-              {input}
-            </div>
-         </div>
+      <div className="form-group">
+        <label className="col-sm-4 control-label">{label}</label>
+        <label className="sr-only" for="search">Search</label>
+        <div className="col-sm-8">
+          {input}
+        </div>
+      </div>
     )
 
   }
